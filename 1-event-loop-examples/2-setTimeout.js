ab6b3b8f1809ffdaccfe4b49dee5e83b34737585
Adonis <adonis.casado@outlook.com>
2024-03-29 09:32:56 -0400

    update

---
+++ 1-event-loop-examples/2-setTimeout.js
@@ -0,0 +1,6 @@
+console.log('first');
+setTimeout(() => {
+  console.log('second')
+}, o)
+console.log('third')
+//completed and exited operating system
--- 10-fa-sync.js
+++ 10-fa-sync.js
@@ -1,5 +1,7 @@
 const { readFileSync, writeFileSync } = require('fs');
+
 console.log('start')
+
 const first = readFileSync('./content/first.txt', 'utf-8');
 const second = readFileSync('./content/second.txt', 'utf-8');
 
